5e0688d18c6ea59df4a5c90f688c19ac0744af2c
Jacob Asper <78604367+20jasper@users.noreply.github.com>
2024-09-08 14:34:54 -0400

    prettier astro (#1)
    * add prettier plugin astro
    
    * format everything
    
    * don't cache prettier

---
+++ .prettierrc.cjs
@@ -0,0 +1,20 @@
+/**
+ * @type {import("prettier").Config}
+ */
+module.exports = {
+	plugins: ['prettier-plugin-astro'],
+	overrides: [
+		{
+			files: '**/*.astro',
+			options: {
+				parser: 'astro',
+			},
+		},
+	],
+
+	semi: true,
+	tabWidth: 2,
+	singleQuote: true,
+	useTabs: true,
+	singleAttributePerLine: true,
+};
--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,13 @@
 // @ts-check
-import { defineConfig } from "astro/config";
-import mdx from "@astrojs/mdx";
+import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
 
-import sitemap from "@astrojs/sitemap";
+import sitemap from '@astrojs/sitemap';
 
-import compressor from "astro-compressor";
+import compressor from 'astro-compressor';
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://example.com",
-  integrations: [mdx(), sitemap(), compressor()],
+	site: 'https://example.com',
+	integrations: [mdx(), sitemap(), compressor()],
 });
