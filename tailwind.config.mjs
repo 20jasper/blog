288e3fbc71a344342b5ad723cdb3cff8bfd59723
Jacob Asper <jacobasper191@gmail.com>
2024-09-09 19:58:04 -0400

    set default font family

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,6 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {
+			fontFamily: {
+				sans: ['Poppins', 'sans-serif'],
+			},
+		},
+	},
 	plugins: [
 		require('@tailwindcss/typography'),
 		function ({ addVariant }) {
