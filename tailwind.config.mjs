bf6031e22990d84a7b9de55890bfe67d74ead7ff
Jacob Asper <jacobasper191@gmail.com>
2024-09-09 12:51:52 -0400

    add custom tailwind variant

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,5 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	plugins: [require('@tailwindcss/typography')],
+	plugins: [
+		require('@tailwindcss/typography'),
+		function ({ addVariant }) {
+			addVariant(
+				'prose-inline-code',
+				'&.prose :where(:not(pre)>code):not(:where([class~="not-prose"] *))',
+			);
+		},
+	],
 };
