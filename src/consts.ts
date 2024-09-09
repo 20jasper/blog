402d61cb7958bd4434ab9bc8636ee0e89336278d
Jacob Asper <jacobasper191@gmail.com>
2024-09-08 22:40:10 -0400

    update blog name

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,3 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
-
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Jacob Asper';
+export const NAME = 'Jacob Asper';
+export const SITE_DESCRIPTION = 'Rust is a must 🦀';
