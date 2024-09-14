d47aedce6d45dc660d79096497550eabd80d7dff
Jacob Asper <jacobasper191@gmail.com>
2024-09-14 15:40:39 -0400

    add reading list

--- src/content/config.ts
+++ src/content/config.ts
@@ -2,15 +2,24 @@ import { defineCollection, z } from 'astro:content';
 
 const blog = defineCollection({
 	type: 'content',
-	// Type-check frontmatter using a schema
 	schema: z.object({
 		title: z.string(),
 		description: z.string(),
-		// Transform string to Date object
 		pubDate: z.coerce.date(),
 		updatedDate: z.coerce.date().optional(),
 		heroImage: z.string().optional(),
 	}),
 });
 
-export const collections = { blog };
+const book = defineCollection({
+	type: 'content',
+	schema: z.object({
+		title: z.string(),
+		author: z.string().optional(),
+		link: z.string().optional(),
+		description: z.string().optional(),
+		readDate: z.coerce.date(),
+	}),
+});
+
+export const collections = { blog, book };
