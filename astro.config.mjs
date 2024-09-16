60adbeb510e7f2506c9796530bfe8ae426522e8a
Jacob Asper <jacobasper191@gmail.com>
2024-09-15 21:26:30 -0400

    use orange theme for code blocks

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,12 +4,18 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import compressor from 'astro-compressor';
 import tailwind from '@astrojs/tailwind';
+import syntaxTheme from './orange-theme';
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://blog.jacobasper.com',
 	redirects: {
 		'/': '/blog',
+	},
+	markdown: {
+		shikiConfig: {
+			theme: syntaxTheme,
+		},
 	},
 	integrations: [mdx(), sitemap(), tailwind(), compressor()],
 });
