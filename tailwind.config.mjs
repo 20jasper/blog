21e7d53b66da4c06a3e7dd3d19e8ebf04f91936e
Jacob Asper <jacobasper191@gmail.com>
2024-09-22 14:34:32 -0400

    change font to arial

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -4,7 +4,7 @@ export default {
 	theme: {
 		extend: {
 			fontFamily: {
-				sans: ['Poppins', 'sans-serif'],
+				sans: ['Arial', 'sans-serif'],
 			},
 			colors: {
 				background: 'rgb(var(--background))',
