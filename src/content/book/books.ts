d286822140cf26a8be763fbd3184e6079bc2e8ba
Jacob Asper <jacobasper191@gmail.com>
2025-05-22 19:22:19 -0400

    add subtitle to joy of react

--- src/content/book/books.ts
+++ src/content/book/books.ts
@@ -136,13 +136,13 @@ export const booksStarted: Book[] = [
 		rating: null,
 	},
 	{
-		title: 'Joy of React',
+		title: "Joy of React (not a book but I dont' care)",
 		author: 'Josh Comeau',
 		readDate: {
 			start: new Date('2025-5-11'),
 			end: null,
 		},
-		rating: null,
+		rating: 5,
 	},
 	{
 		title: 'Effective Rust',
