9871f4e497bcb3b9db80cb832d9e86c6c85a26d3
Jacob Asper <jacobasper191@gmail.com>
2025-12-31 21:34:03 -0500

    uncomment questions

--- src/content/questions/questions.ts
+++ src/content/questions/questions.ts
@@ -9,203 +9,203 @@ export type Question = {
 };
 
 export const questions: Question[] = [
-	// {
-	// 	json: '{  "am i"    : "valid?"  }',
-	// 	explanation: 'Whitespace is allowed between all JSON tokens',
-	// 	citation: 'https://datatracker.ietf.org/doc/html/rfc8259#section-2',
-	// 	answer: 'valid',
-	// },
-	// {
-	// 	json: '{ hello: "world" }',
-	// 	explanation: "Object keys must be strings—this isn't JavaScript!",
-	// 	citation: 'https://datatracker.ietf.org/doc/html/rfc8259#section-4',
-	// 	answer: 'invalid',
-	// },
-	// {
-	// 	json: '[',
-	// 	explanation: 'Arrays must be closed with a closing square bracket (`]`)',
-	// 	citation: 'https://datatracker.ietf.org/doc/html/rfc8259#section-5',
-	// 	answer: 'invalid',
-	// },
-	// {
-	// 	json: 'true',
-	// 	explanation:
-	// 		'Top level scalars are chill. `true`, `false`, `null`, objects, numbers, strings and arrays are all valid top level JSON values as of RFC7158',
-	// 	citation: 'https://datatracker.ietf.org/doc/html/rfc8259#section-3',
-	// 	answer: 'valid',
-	// },
-	// {
-	// 	json: 'True',
-	// 	explanation:
-	// 		"Literal names must be exactly `true`, `false`, or `null`. I'm sorry Python and Haskell devs",
-	// 	citation: 'https://datatracker.ietf.org/doc/html/rfc8259#section-3',
-	// 	answer: 'invalid',
-	// },
-	// {
-	// 	json: '10',
-	// 	explanation: 'Scalars are still chill. 10 is a valid integer',
-	// 	citation: 'https://datatracker.ietf.org/doc/html/rfc8259#section-6',
-	// 	answer: 'valid',
-	// },
-	// {
-	// 	json: '-10',
-	// 	explanation: 'Negative numbers are cool',
-	// 	citation: 'https://datatracker.ietf.org/doc/html/rfc8259#section-6',
-	// 	answer: 'valid',
-	// },
-	// {
-	// 	json: '+10',
-	// 	explanation:
-	// 		"Leading plus sign on integers is not good 😔. This still isn't JavaScript",
-	// 	citation: 'https://datatracker.ietf.org/doc/html/rfc8259#section-6',
-	// 	answer: 'invalid',
-	// },
-	// {
-	// 	json: '0006',
-	// 	explanation: 'Leading zeros are not allowed in integers',
-	// 	citation: 'https://datatracker.ietf.org/doc/html/rfc8259#section-6',
-	// 	answer: 'invalid',
-	// },
-	// {
-	// 	json: '6.0000',
-	// 	explanation:
-	// 		'Decimal numbers with fractional part are valid. Leading zeros signify precision here',
-	// 	citation: 'https://datatracker.ietf.org/doc/html/rfc8259#section-6',
-	// 	answer: 'valid',
-	// },
-	// {
-	// 	json: '0x10',
-	// 	explanation:
-	// 		"Hexadecimal is not allowed in numbers. If you thought this was multiplication—that's not allowed either",
-	// 	citation: 'https://datatracker.ietf.org/doc/html/rfc8259#section-6',
-	// 	answer: 'invalid',
-	// },
-	// {
-	// 	json: '0.00000E0000',
-	// 	explanation:
-	// 		"Exponent part may include leading zeros. I don't know why you'd do that but you can I guess",
-	// 	citation: 'https://datatracker.ietf.org/doc/html/rfc8259#section-6',
-	// 	answer: 'valid',
-	// },
-	// {
-	// 	json: '-0',
-	// 	explanation: 'Negative zero is allowed',
-	// 	citation: 'https://datatracker.ietf.org/doc/html/rfc8259#section-6',
-	// 	answer: 'valid',
-	// },
-	// {
-	// 	json: '5.',
-	// 	explanation: 'Fractional part must have one or more digits',
-	// 	citation: 'https://datatracker.ietf.org/doc/html/rfc8259#section-6',
-	// 	answer: 'invalid',
-	// },
-	// {
-	// 	json: '28828283828383838288282282828282828282828282828387374747474747474747447473733773737377373737372882818192.093290230990392399230239039290',
-	// 	explanation:
-	// 		'IEEE 754 float doubles are recommended, but parsers can choose to accept arbitrarily high precision or accept very low precision. Does that mean pretty much any syntactically valid number is maybe valid? Yep—still not marking every number as maybe valid though',
-	// 	citation: 'https://datatracker.ietf.org/doc/html/rfc8259#section-6',
-	// 	answer: 'maybe valid',
-	// },
-	// {
-	// 	json: 'NaN',
-	// 	explanation:
-	// 		'`NaN` is not a valid JSON number literal. Despite recommending using the IEEE 754 float double range, special values like `NaN` are forbidden. Obligatory "this isn\'t JavaScript"',
-	// 	citation: 'https://datatracker.ietf.org/doc/html/rfc8259#section-6',
-	// 	answer: 'invalid',
-	// },
-	// {
-	// 	json: '/* comment */ {"a":1}',
-	// 	explanation:
-	// 		"Comments are not part of JSON. Parsers are technically free to accept extensions, but I'm still not counting this as valid. Fight me",
-	// 	citation: 'https://datatracker.ietf.org/doc/html/rfc8259#section-9',
-	// 	answer: 'invalid',
-	// },
-	// {
-	// 	json: '"line\u2028separator"',
-	// 	explanation:
-	// 		'`U+2028` and `U+2029` may appear in JSON strings. Fun fact, JavaScript does not allow these characters, so sorry to say, JSON is not a strict subset of JavaScript despite the goals of the format',
-	// 	citation: 'https://datatracker.ietf.org/doc/html/rfc8259#section-7',
-	// 	answer: 'valid',
-	// },
-	// {
-	// 	json: '[10,]',
-	// 	explanation:
-	// 		'Trailing commas aren\'t valid even if many parsers decide to accept them. Obligatory "I-know-extensions-exist-fight-me"',
-	// 	citation: 'https://datatracker.ietf.org/doc/html/rfc8259#section-5',
-	// 	answer: 'invalid',
-	// },
-	// {
-	// 	json: '[[[[[[[[[[[[[[[[[[[[\n[[[[[[[[[[[[[[[[[[[[\n"this is balanced"\n]]]]]]]]]]]]]]]]]]]]\n]]]]]]]]]]]]]]]]]]]]\n',
-	// 	explanation:
-	// 		'Parsers can set limits on nesting and input size. Does this mean almost any input is maybe valid? Yep. Am I going to change the rest of the quiz? Nah',
-	// 	citation: 'https://datatracker.ietf.org/doc/html/rfc8259',
-	// 	answer: 'maybe valid',
-	// },
-	// {
-	// 	json: '{ "hi": null, "hi": null }',
-	// 	explanation:
-	// 		"You sure can have duplicate keys, but please don't unless you really like interoperability issues.",
-	// 	citation: 'https://datatracker.ietf.org/doc/html/rfc8259#section-4',
-	// 	answer: 'valid',
-	// },
-	// {
-	// 	json: '"pretend I\'m UTF-8 encoded"',
-	// 	explanation: 'UTF-8 is the default encoding and very swell',
-	// 	citation: 'https://datatracker.ietf.org/doc/html/rfc8259#section-8.1',
-	// 	answer: 'valid',
-	// },
-	// {
-	// 	json: '"pretend I\'m UTF-16 encoded"',
-	// 	explanation:
-	// 		"UTF-16 is allowed in internal systems that don't go over the network",
-	// 	citation: 'https://datatracker.ietf.org/doc/html/rfc8259#section-8.1',
-	// 	answer: 'maybe valid',
-	// },
-	// {
-	// 	json: '"pretend I\'m UTF-32 encoded"',
-	// 	explanation:
-	// 		"UTF-32 is allowed in internal systems that don't go over the network",
-	// 	citation: 'https://datatracker.ietf.org/doc/html/rfc8259#section-8.1',
-	// 	answer: 'maybe valid',
-	// },
-	// {
-	// 	json: '"pretend I\'m UTF-7 encoded"',
-	// 	explanation: 'UTF-7 is not very swell',
-	// 	citation: 'https://datatracker.ietf.org/doc/html/rfc8259#section-8.1',
-	// 	answer: 'invalid',
-	// },
-	// {
-	// 	json: '\ufeff"prefixed by byte order marker (BOM)"',
-	// 	explanation:
-	// 		'A leading byte order mark (BOM) is allowed in non-network-transmitted JSON and can be ignored in network-transmitted JSON',
-	// 	citation: 'https://datatracker.ietf.org/doc/html/rfc8259#section-8.1',
-	// 	answer: 'maybe valid',
-	// },
-	// {
-	// 	json: '"\\u20AC euro sign escape"',
-	// 	explanation: 'Unicode escapes are valid in strings.',
-	// 	citation: 'https://datatracker.ietf.org/doc/html/rfc8259#section-7',
-	// 	answer: 'valid',
-	// },
-	// {
-	// 	json: '"\\n"',
-	// 	explanation: 'Escaped newline inside a string is valid.',
-	// 	citation: 'https://datatracker.ietf.org/doc/html/rfc8259#section-7',
-	// 	answer: 'valid',
-	// },
-	// {
-	// 	json: '"are unescaped newlines\n allowed?"',
-	// 	explanation: "Control characters like newlines aren't allowed in strings",
-	// 	citation: 'https://datatracker.ietf.org/doc/html/rfc8259#section-7',
-	// 	answer: 'invalid',
-	// },
-	// {
-	// 	json: '"\\u000A unicode escaped newline"',
-	// 	explanation:
-	// 		"You're allowed to use Unicode escapes for newlines too! In fact, any character can be Unicode escaped",
-	// 	citation: 'https://datatracker.ietf.org/doc/html/rfc8259#section-7',
-	// 	answer: 'valid',
-	// },
+	{
+		json: '{  "am i"    : "valid?"  }',
+		explanation: 'Whitespace is allowed between all JSON tokens',
+		citation: 'https://datatracker.ietf.org/doc/html/rfc8259#section-2',
+		answer: 'valid',
+	},
+	{
+		json: '{ hello: "world" }',
+		explanation: "Object keys must be strings—this isn't JavaScript!",
+		citation: 'https://datatracker.ietf.org/doc/html/rfc8259#section-4',
+		answer: 'invalid',
+	},
+	{
+		json: '[',
+		explanation: 'Arrays must be closed with a closing square bracket (`]`)',
+		citation: 'https://datatracker.ietf.org/doc/html/rfc8259#section-5',
+		answer: 'invalid',
+	},
+	{
+		json: 'true',
+		explanation:
+			'Top level scalars are chill. `true`, `false`, `null`, objects, numbers, strings and arrays are all valid top level JSON values as of RFC7158',
+		citation: 'https://datatracker.ietf.org/doc/html/rfc8259#section-3',
+		answer: 'valid',
+	},
+	{
+		json: 'True',
+		explanation:
+			"Literal names must be exactly `true`, `false`, or `null`. I'm sorry Python and Haskell devs",
+		citation: 'https://datatracker.ietf.org/doc/html/rfc8259#section-3',
+		answer: 'invalid',
+	},
+	{
+		json: '10',
+		explanation: 'Scalars are still chill. 10 is a valid integer',
+		citation: 'https://datatracker.ietf.org/doc/html/rfc8259#section-6',
+		answer: 'valid',
+	},
+	{
+		json: '-10',
+		explanation: 'Negative numbers are cool',
+		citation: 'https://datatracker.ietf.org/doc/html/rfc8259#section-6',
+		answer: 'valid',
+	},
+	{
+		json: '+10',
+		explanation:
+			"Leading plus sign on integers is not good 😔. This still isn't JavaScript",
+		citation: 'https://datatracker.ietf.org/doc/html/rfc8259#section-6',
+		answer: 'invalid',
+	},
+	{
+		json: '0006',
+		explanation: 'Leading zeros are not allowed in integers',
+		citation: 'https://datatracker.ietf.org/doc/html/rfc8259#section-6',
+		answer: 'invalid',
+	},
+	{
+		json: '6.0000',
+		explanation:
+			'Decimal numbers with fractional part are valid. Leading zeros signify precision here',
+		citation: 'https://datatracker.ietf.org/doc/html/rfc8259#section-6',
+		answer: 'valid',
+	},
+	{
+		json: '0x10',
+		explanation:
+			"Hexadecimal is not allowed in numbers. If you thought this was multiplication—that's not allowed either",
+		citation: 'https://datatracker.ietf.org/doc/html/rfc8259#section-6',
+		answer: 'invalid',
+	},
+	{
+		json: '0.00000E0000',
+		explanation:
+			"Exponent part may include leading zeros. I don't know why you'd do that but you can I guess",
+		citation: 'https://datatracker.ietf.org/doc/html/rfc8259#section-6',
+		answer: 'valid',
+	},
+	{
+		json: '-0',
+		explanation: 'Negative zero is allowed',
+		citation: 'https://datatracker.ietf.org/doc/html/rfc8259#section-6',
+		answer: 'valid',
+	},
+	{
+		json: '5.',
+		explanation: 'Fractional part must have one or more digits',
+		citation: 'https://datatracker.ietf.org/doc/html/rfc8259#section-6',
+		answer: 'invalid',
+	},
+	{
+		json: '28828283828383838288282282828282828282828282828387374747474747474747447473733773737377373737372882818192.093290230990392399230239039290',
+		explanation:
+			'IEEE 754 float doubles are recommended, but parsers can choose to accept arbitrarily high precision or accept very low precision. Does that mean pretty much any syntactically valid number is maybe valid? Yep—still not marking every number as maybe valid though',
+		citation: 'https://datatracker.ietf.org/doc/html/rfc8259#section-6',
+		answer: 'maybe valid',
+	},
+	{
+		json: 'NaN',
+		explanation:
+			'`NaN` is not a valid JSON number literal. Despite recommending using the IEEE 754 float double range, special values like `NaN` are forbidden. Obligatory "this isn\'t JavaScript"',
+		citation: 'https://datatracker.ietf.org/doc/html/rfc8259#section-6',
+		answer: 'invalid',
+	},
+	{
+		json: '/* comment */ {"a":1}',
+		explanation:
+			"Comments are not part of JSON. Parsers are technically free to accept extensions, but I'm still not counting this as valid. Fight me",
+		citation: 'https://datatracker.ietf.org/doc/html/rfc8259#section-9',
+		answer: 'invalid',
+	},
+	{
+		json: '"line\u2028separator"',
+		explanation:
+			'`U+2028` and `U+2029` may appear in JSON strings. Fun fact, JavaScript does not allow these characters, so sorry to say, JSON is not a strict subset of JavaScript despite the goals of the format',
+		citation: 'https://datatracker.ietf.org/doc/html/rfc8259#section-7',
+		answer: 'valid',
+	},
+	{
+		json: '[10,]',
+		explanation:
+			'Trailing commas aren\'t valid even if many parsers decide to accept them. Obligatory "I-know-extensions-exist-fight-me"',
+		citation: 'https://datatracker.ietf.org/doc/html/rfc8259#section-5',
+		answer: 'invalid',
+	},
+	{
+		json: '[[[[[[[[[[[[[[[[[[[[\n[[[[[[[[[[[[[[[[[[[[\n"this is balanced"\n]]]]]]]]]]]]]]]]]]]]\n]]]]]]]]]]]]]]]]]]]]\n',
+		explanation:
+			'Parsers can set limits on nesting and input size. Does this mean almost any input is maybe valid? Yep. Am I going to change the rest of the quiz? Nah',
+		citation: 'https://datatracker.ietf.org/doc/html/rfc8259',
+		answer: 'maybe valid',
+	},
+	{
+		json: '{ "hi": null, "hi": null }',
+		explanation:
+			"You sure can have duplicate keys, but please don't unless you really like interoperability issues.",
+		citation: 'https://datatracker.ietf.org/doc/html/rfc8259#section-4',
+		answer: 'valid',
+	},
+	{
+		json: '"pretend I\'m UTF-8 encoded"',
+		explanation: 'UTF-8 is the default encoding and very swell',
+		citation: 'https://datatracker.ietf.org/doc/html/rfc8259#section-8.1',
+		answer: 'valid',
+	},
+	{
+		json: '"pretend I\'m UTF-16 encoded"',
+		explanation:
+			"UTF-16 is allowed in internal systems that don't go over the network",
+		citation: 'https://datatracker.ietf.org/doc/html/rfc8259#section-8.1',
+		answer: 'maybe valid',
+	},
+	{
+		json: '"pretend I\'m UTF-32 encoded"',
+		explanation:
+			"UTF-32 is allowed in internal systems that don't go over the network",
+		citation: 'https://datatracker.ietf.org/doc/html/rfc8259#section-8.1',
+		answer: 'maybe valid',
+	},
+	{
+		json: '"pretend I\'m UTF-7 encoded"',
+		explanation: 'UTF-7 is not very swell',
+		citation: 'https://datatracker.ietf.org/doc/html/rfc8259#section-8.1',
+		answer: 'invalid',
+	},
+	{
+		json: '\ufeff"prefixed by byte order marker (BOM)"',
+		explanation:
+			'A leading byte order mark (BOM) is allowed in non-network-transmitted JSON and can be ignored in network-transmitted JSON',
+		citation: 'https://datatracker.ietf.org/doc/html/rfc8259#section-8.1',
+		answer: 'maybe valid',
+	},
+	{
+		json: '"\\u20AC euro sign escape"',
+		explanation: 'Unicode escapes are valid in strings.',
+		citation: 'https://datatracker.ietf.org/doc/html/rfc8259#section-7',
+		answer: 'valid',
+	},
+	{
+		json: '"\\n"',
+		explanation: 'Escaped newline inside a string is valid.',
+		citation: 'https://datatracker.ietf.org/doc/html/rfc8259#section-7',
+		answer: 'valid',
+	},
+	{
+		json: '"are unescaped newlines\n allowed?"',
+		explanation: "Control characters like newlines aren't allowed in strings",
+		citation: 'https://datatracker.ietf.org/doc/html/rfc8259#section-7',
+		answer: 'invalid',
+	},
+	{
+		json: '"\\u000A unicode escaped newline"',
+		explanation:
+			"You're allowed to use Unicode escapes for newlines too! In fact, any character can be Unicode escaped",
+		citation: 'https://datatracker.ietf.org/doc/html/rfc8259#section-7',
+		answer: 'valid',
+	},
 
 	{
 		json: '" unescaped DEL character"',
