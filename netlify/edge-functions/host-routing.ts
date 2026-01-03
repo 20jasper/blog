76ba19560b7c298b783aa577c484f4f9ca556c56
Jacob Asper <jacobasper191@gmail.com>
2026-01-03 00:48:37 -0500

    add logging to routing and allow static files from any domain

--- netlify/edge-functions/host-routing.ts
+++ netlify/edge-functions/host-routing.ts
@@ -1,28 +1,44 @@
 import type { EdgeFunction } from '@netlify/edge-functions';
 
 const JSONWTF_BASE = new URL('https://jsonwtf.org');
+const JACOBASPER = new URL('https://jacobasper.com');
+
+const matchesPath = (expected: string, actual: string): boolean =>
+	expected === actual || expected + '/' === actual;
+
+const FILE_EXTENSIONS = 
+[".css", ".js", ".webp", ".png", ".jpg", ".jpeg", ".gif", ".svg", ".ico"];
+
+const isFile = (path: string) =>
+	path.startsWith('/_astro/') ||
+	FILE_EXTENSIONS.some(ext => path.endsWith(ext));
 
 const handler: EdgeFunction = (request, _context) => {
 	const url = new URL(request.url);
 	const host = url.hostname;
 	const path = url.pathname;
+	console.log({ host, path });
+
+	if (isFile(path)) {
+		return;
+	}
 
 	// only serve jsonwtf urls from jsonwtf.org
-	if (host === 'jsonwtf.org') {
+	if (host === JSONWTF_BASE.hostname) {
 		if (path === '/') {
 			return Response.redirect(new URL('/quiz', JSONWTF_BASE), 301);
 		}
-		if (path !== '/quiz') {
-			return new Response('Not Found', { status: 404 });
+		if (!matchesPath('/quiz', path)) {
+			return Response.redirect(new URL(path, JACOBASPER), 301);
 		}
 	}
 
 	// serve rest from jacobasper.com
-	if (host === 'jacobasper.com') {
+	if (host === JACOBASPER.hostname) {
 		if (path === '/' || path.startsWith('/projects')) {
 			return new URL('/blog', request.url);
 		}
-		if (path === '/quiz') {
+		if (matchesPath('/quiz', path)) {
 			return Response.redirect(new URL('/quiz', JSONWTF_BASE), 301);
 		}
 	}
