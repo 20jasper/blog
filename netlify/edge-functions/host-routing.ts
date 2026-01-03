e42d07752f9ae4b447c4e41ee5e2ee076740ceed
Jacob Asper <jacobasper191@gmail.com>
2026-01-03 01:28:32 -0500

    omit static assets at the root

--- netlify/edge-functions/host-routing.ts
+++ netlify/edge-functions/host-routing.ts
@@ -1,4 +1,4 @@
-import type { EdgeFunction } from '@netlify/edge-functions';
+import type { Config, EdgeFunction } from '@netlify/edge-functions';
 
 const JSONWTF_BASE = new URL('https://jsonwtf.org');
 const JACOBASPER = new URL('https://jacobasper.com');
@@ -6,12 +6,9 @@ const JACOBASPER = new URL('https://jacobasper.com');
 const matchesPath = (expected: string, actual: string): boolean =>
 	expected === actual || expected + '/' === actual;
 
-const FILE_EXTENSIONS = 
-[".css", ".js", ".webp", ".png", ".jpg", ".jpeg", ".gif", ".svg", ".ico"];
-
-const isFile = (path: string) =>
-	path.startsWith('/_astro/') ||
-	FILE_EXTENSIONS.some(ext => path.endsWith(ext));
+const redirect = (url: URL) => {
+	return Response.redirect(url, 301);
+};
 
 const handler: EdgeFunction = (request, _context) => {
 	const url = new URL(request.url);
@@ -19,17 +16,13 @@ const handler: EdgeFunction = (request, _context) => {
 	const path = url.pathname;
 	console.log({ host, path });
 
-	if (isFile(path)) {
-		return;
-	}
-
 	// only serve jsonwtf urls from jsonwtf.org
 	if (host === JSONWTF_BASE.hostname) {
 		if (path === '/') {
-			return Response.redirect(new URL('/quiz', JSONWTF_BASE), 301);
+			return redirect(new URL('/quiz', JSONWTF_BASE));
 		}
 		if (!matchesPath('/quiz', path)) {
-			return Response.redirect(new URL(path, JACOBASPER), 301);
+			return redirect(new URL(path, JACOBASPER));
 		}
 	}
 
@@ -39,7 +32,7 @@ const handler: EdgeFunction = (request, _context) => {
 			return new URL('/blog', request.url);
 		}
 		if (matchesPath('/quiz', path)) {
-			return Response.redirect(new URL('/quiz', JSONWTF_BASE), 301);
+			return redirect(new URL('/quiz', JSONWTF_BASE));
 		}
 	}
 
@@ -47,3 +40,20 @@ const handler: EdgeFunction = (request, _context) => {
 };
 
 export default handler;
+
+export const config: Config = {
+	cache: 'manual',
+	path: '/*',
+	excludedPattern: [
+		'/_astro/*',
+		'*.css',
+		'*.js',
+		'*.webp',
+		'*.png',
+		'*.jpg',
+		'*.jpeg',
+		'*.gif',
+		'*.svg',
+		'*.ico',
+	],
+};
